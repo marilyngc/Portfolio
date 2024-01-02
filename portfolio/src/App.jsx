6ec804285d617881e1484d2fab1be9c3f874d1ff
marilyn <marilyncelisgutierrez@gmail.com>
2024-01-02 11:50:15 -0300

    arreglé la dirrecion de home.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Home } from './components/page/home.jsx'
+import { Home } from "./components/page/Home.jsx";
 import './App.css'
 import "./scss/styles.scss";
 import './index.css'; // Asegúrate de que la ruta sea correcta
