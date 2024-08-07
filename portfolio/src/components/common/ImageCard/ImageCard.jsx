fc5a75277eebea6f75180d47237ba23a829dd484
Marilyn Celis <marilyncelisgutierrez@gmail.com>
2024-08-07 12:18:56 -0300

    modificacion de id de herramientas

--- portfolio/src/components/common/ImageCard/ImageCard.jsx
+++ portfolio/src/components/common/ImageCard/ImageCard.jsx
@@ -59,14 +59,13 @@ export const ImageCard = () => {
                 {proyect.tools.map((tool, index) => (
                   <div key={index} className="relative group/tooltip">
                     <img
-                      src={tool}
+                      src={tool.src}
                       alt={`Tool ${index + 1}`}
                       className="w-7 h-6"
                     />
                     {proyect.event.map((event, eventIndex) => {
-                      const toolName = tool.split("/").pop().split(".")[0]; // Extraer el nombre de la herramienta de la ruta de la imagen
 
-                      if (event.toUpperCase() === toolName.toUpperCase()) {
+                      if (event.toUpperCase() === tool.id.toUpperCase()) {
                         return (
                           <span
                             key={eventIndex}
@@ -80,7 +79,10 @@ export const ImageCard = () => {
                           </span>
                         );
                       }
-                      return null;
+                      else{
+                        return null;
+                      };
+                      
                     })}
                   </div>
                 ))}
@@ -141,25 +143,23 @@ export const ImageCard = () => {
                   {proyect.tools.map((tool,index) =>
                   <div key={index} className="relative group/tooltip">
                     <img
-                      src={tool}
+                      src={tool.src}
                       alt={`Tool ${index + 1}`}
                       className="w-7 h-6"
                     />
 
                  
                     {proyect.event.map((evento,index) =>{
-                        const toolName = tool.split("/").pop().split(".")[0].toLowerCase().trim(); // Extraer el nombre de la herramienta de la ruta de la imagen
-                        // console.log("herramienta:",toolName)
-
-                        const eventName = evento.toLowerCase().trim()
-                        // console.log("evento: ",eventName)
+                        const toolName = tool.id.toLowerCase();
+                        const eventName = evento.toLowerCase()
+                        
                         if(eventName == toolName) {
                           console.log("herramienta:",toolName,eventName)
                           // console.log("evento:",eventName)
                             return(
 
                               <span key={index} className="absolute pointer-events-none transition-all opacity-0 z-20 bottom-full -translate-y-0 py-1 px-1.5 text-xs left-1/2 -translate-x-1/2 rounded-md whitespace-nowrap text-gray-200 bg-gray-800 dark:bg-white dark:text-gray-700 before:content-[''] before:absolute before:bg-gray-800 before:rounded-sm before:w-2.5 before:rotate-45 before:h-2.5 before:-bottom-1 before:-z-10 before:left-1/2 before:-translate-x-1/2 before:dark:bg-white before:dark:gray-800 group-hover/tooltip:opacity-100 group-hover/tooltip:-translate-y-3">
-                                  {eventName}
+                                  {evento}
                                   
                                </span>
                                
@@ -167,8 +167,8 @@ export const ImageCard = () => {
                         
                        }
                        else{
-                        console.log(`No match for: ${toolName} (tool) and ${eventName} (event)`); // Registro de no coincidencia
-                       }
+                        return null;
+                       };
                       }
                     
                     )}
--- portfolio/src/data/MOCK_DATA.jsx
+++ portfolio/src/data/MOCK_DATA.jsx
@@ -1,86 +1,159 @@
 import home from "../assets/home.png";
-import ecommerce from "../assets/ecommerce.png"
-import millonario from "../assets/menu_millonario.png"
-import react from "../assets//tools/react.svg"
-import javascript from "../assets//tools/javascript.svg"
-import sass from "../assets/tools/sass.svg"
-import bootstrap from "../assets/tools/bootstrap.svg"
-import firebase from "../assets/tools/firebase.svg"
-import socket from "../assets/tools/socket.svg"
-import handlebars from "../assets/tools/handlebars.svg"
-import express from "../assets/tools/express.svg"
-import python from "../assets/tools/python.svg"
-import pygame from "../assets/tools/pygame.png"
-import { FaLinkedin,FaGithub  } from "react-icons/fa6";
-
+import ecommerce from "../assets/ecommerce.png";
+import millonario from "../assets/menu_millonario.png";
+import react from "../assets//tools/react.svg";
+import javascript from "../assets//tools/javascript.svg";
+import sass from "../assets/tools/sass.svg";
+import bootstrap from "../assets/tools/bootstrap.svg";
+import firebase from "../assets/tools/firebase.svg";
+import socket from "../assets/tools/socket.svg";
+import handlebars from "../assets/tools/handlebars.svg";
+import express from "../assets/tools/express.svg";
+import python from "../assets/tools/python.svg";
+import pygame from "../assets/tools/pygame.png";
+import { FaLinkedin, FaGithub } from "react-icons/fa6";
 
 export const DataProyects = [
-    {
-        img: home,
-        title: "Tu mejor amigo",
-        description: "Sitio inspirado en TuMejorAmigo. El sitio web permite que los dueños encuentren cuidadores confiables para sus animales.",
-        tools: [react , javascript, bootstrap, sass],
-        event: ["Sass","Javascript","React","Bootstrap"],
-        link: "https://marilyngc.github.io/TuMejorAmigo//"
+  {
+    img: home,
+    title: "Tu mejor amigo",
+    description:
+      "Sitio inspirado en TuMejorAmigo. El sitio web permite que los dueños encuentren cuidadores confiables para sus animales.",
+    tools: [
+      {
+        id: "react",
+        src: react,
+      },
+      {
+        id: "javascript",
+        src: javascript,
+      },
+      {
+        id: "bootstrap",
+        src: bootstrap,
+      },
+      {
+        id: "sass",
+        src: sass,
+      },
+    ],
+    event: ["Sass", "Javascript", "React", "Bootstrap"],
+    link: "https://marilyngc.github.io/TuMejorAmigo//",
+  },
+  {
+    img: ecommerce,
+    title: "E-commerce",
+    description:
+      "Proyecto final realizado en CoderHouse quedando en el TOP 10 de la comisión.",
+    tools: [
+        {
+          id: "react",
+          src: react,
+        },
+        {
+          id: "javascript",
+          src: javascript,
+        },
+        
+        {
+          id: "firebase",
+          src: firebase,
+        },
 
-
-
-    },
-    {
-        img: ecommerce,
-        title: "E-commerce",
-        description: "Proyecto final realizado en CoderHouse quedando en el TOP 10 de la comisión.",
-        tools: [react,javascript ,firebase, bootstrap, sass],
-        event: ["Sass","Javascript","React","Firebase","Bootstrap"],
-        link: "https://ecommerce-leyb.vercel.app"
-
-
-
-    },
-    {
-        img: millonario,
-        title: "¿Quién quiere ser millonario?",
-        description: "Juego de trivia que hace referencia al famoso programa de televisión.",
-        tools: [python,pygame],
-        event: ["Pygame","Python"],
-        link: "https://github.com/marilyngc/Quien_quiere_ser_millonario_juego"
-
-
-
-    }
-   
-
-
+        {
+          id: "bootstrap",
+          src: bootstrap,
+        },
+        {
+          id: "sass",
+          src: sass,
+        },
+      ],
+    event: ["Sass", "Javascript", "React", "Firebase", "Bootstrap"],
+    link: "https://ecommerce-leyb.vercel.app",
+  },
+  {
+    img: millonario,
+    title: "¿Quién quiere ser millonario?",
+    description:
+      "Juego de trivia que hace referencia al famoso programa de televisión.",
+    tools: [
+        {
+          id: "python",
+          src: python,
+        },
+        {
+          id: "pygame",
+          src: pygame,
+        },
+        
+      ],
+    event: ["Pygame", "Python"],
+    link: "https://github.com/marilyngc/Quien_quiere_ser_millonario_juego",
+  },
 ];
 
 export const DataProyectNotImage = [
-    {
-        title:"Chat Server",
-        description: "Un ejemplo mínimo de programación para un servidor de chat.",
-        tools: [handlebars,socket,express],
-        event: ["Handlebars","Socket","React","Express"],
-        link : "https://github.com/marilyngc/Chat",
-    },
-    {  
-        title: "Wellness Center",
-        description: "Plataforma dedicada a ofrecer una experiencia centrada a la salud física.",
-        tools: [ react,javascript,bootstrap,sass, ],
-        event: ["Sass","Javascript","React","Bootstrap"],
-        link: "https://wellnesscenterspaygym.com"
+  {
+    title: "Chat Server",
+    description: "Un ejemplo mínimo de programación para un servidor de chat.",
+    tools: [
+        {
+          id: "handlebars",
+          src: handlebars,
+        },
+        {
+          id: "socket",
+          src: socket,
+        },
+        {
+          id: "express",
+          src: express,
+        },
+        
+      ],
+   
+    event: ["Handlebars", "Socket", "React", "Express"],
+    link: "https://github.com/marilyngc/Chat",
+  },
+  {
+    title: "Wellness Center",
+    description:
+      "Plataforma dedicada a ofrecer una experiencia centrada a la salud física.",
+    tools: [
+        {
+          id: "react",
+          src: react,
+        },
+        {
+          id: "javascript",
+          src: javascript,
+        },
+        {
+          id: "bootstrap",
+          src: bootstrap,
+        },
+        {
+          id: "sass",
+          src: sass,
+        },
+        
+      ],
 
-}
-]
+    event: ["Sass", "Javascript", "React", "Bootstrap"],
+    link: "https://wellnesscenterspaygym.com",
+  },
+];
 
 export const SocialNetworks = [
-    {
-        title:"Linkedin",
-        link:"https://www.linkedin.com/in/marilyn-celis-developer573/",
-        icon:<FaLinkedin className="text-2xl"/>,
-        
-    },
-    {
-        title:"GitHub",
-        link:"https://github.com/marilyngc",
-        icon:<FaGithub  className="text-2xl"/>,
-    },
-]
+  {
+    title: "Linkedin",
+    link: "https://www.linkedin.com/in/marilyn-celis-developer573/",
+    icon: <FaLinkedin className="text-2xl" />,
+  },
+  {
+    title: "GitHub",
+    link: "https://github.com/marilyngc",
+    icon: <FaGithub className="text-2xl" />,
+  },
+];
